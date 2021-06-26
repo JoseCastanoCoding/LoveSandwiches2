cdac5fdaeb97f546e1aaaf84d670ef95a9b88aca
JoseCastanoCoding <j.albertocast@gmail.com>
2021-06-26 22:00:16 +0000

    main function added

--- run.py
+++ run.py
@@ -62,6 +62,19 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+def calculate_surplus_data(sales_row):
+    
+
+
+def main():
+    """
+    Run all program functions.
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+
+
+print("Welcome to Love Sandwiches Data Automation")
+main()
